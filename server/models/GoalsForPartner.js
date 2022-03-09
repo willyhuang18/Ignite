03d89e9f20fdc6088ca9f261a0e1d73feb49db02
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-08 16:03:58 -0800

    started on properties for Goals Schema

--- server/models/GoalsForPartner.js
+++ server/models/GoalsForPartner.js
@@ -4,5 +4,16 @@ const { Schema, Types } = require("mongoose");
 // Schema for Partner Goals
 const partnerGoalsSchema = new Schema(
     // properties
-    {},
+    {
+        goalTitle:{
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 100,
+        },
+        userRef:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }
+    }
 );
