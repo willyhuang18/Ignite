b177bc7c8884b7a9dedf7e4c4e91d776e27aead4
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-08 16:46:01 -0800

    foreshawdoed the idea of virtual counter

--- server/models/GoalsForPartner.js
+++ server/models/GoalsForPartner.js
@@ -14,6 +14,21 @@ const partnerGoalsSchema = new Schema(
         userRef:{
             type: Schema.Types.ObjectId,
             ref: "User",
-        }
+        },
+
+    },
+    // Mongoose supports transforming Objects after querying MongoDb: toJSON 
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,
     }
 );
+// |--- To Be disccussed but the idea is open ----|
+// partnerGoalsSchema
+// .virtual('GoalCounter')
+// //getter
+// .get()
