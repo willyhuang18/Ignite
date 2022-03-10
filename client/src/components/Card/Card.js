ce8f2d2c38d435eb3980be43158a55fbd8748079
wiily <willyhuang18@gmail.com>
2022-03-09 23:17:35 -0800

    saving the image in the local storage

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -9,23 +9,31 @@ $(document).ready(function() {
   var readURL = function(input) {
       if (input.files && input.files[0]) {
           var reader = new FileReader();
-
           reader.onload = function (e) {
               $('.profile-pic').attr('src', e.target.result);
           }
-  
+          reader.addEventListener('load', ()=>{
+            localStorage.setItem('recent-image',reader.result)
+          })
           reader.readAsDataURL(input.files[0]);
       }
   }
   
-
   $(".file-upload").on('change', function(){
-      readURL(this);
+      readURL(this); 
   });
   
   $(".upload-button").on('click', function() {
      $(".file-upload").click();
   });
+  
+  $(window).on("load",() =>{
+    const recentImageDataUrl = localStorage.getItem('recent-image')
+    
+    if(recentImageDataUrl){
+      $('.profile-pic').attr('src',recentImageDataUrl);
+    }
+  })
 });
 
 function Card() {
