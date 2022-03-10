a6d6ca68648a9d84d685a8776314bd70e434f13e
wiily <willyhuang18@gmail.com>
2022-03-09 16:40:36 -0800

    adding dashboard component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
-import Card from '../components/Card/Card'
-import Question from '../components/Question/Question';
+import Dashboard from '../components/Dashboard';
 
 const Home = () => {
   return (
     <main >
       <div className="flex-row ">
         <div className="col-12 col-md-10 mb-3 p-3">
-        <Card />
-        
-        <Question />
+        <Dashboard />
         </div>
       </div>
     </main>
