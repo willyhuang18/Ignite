f93a5e5160bb62878b9f7e828e7f03f8bf6789f0
Leslie Patino <lesliejpatino@gmail.com>
2022-03-11 14:57:48 -0800

    used the map method to display questionnaire questions one by one

--- client/src/components/Questions/Questionnaire.js
+++ client/src/components/Questions/Questionnaire.js
@@ -3,16 +3,16 @@ import React from 'react';
 import './Questionnaire.css'
 
 
-export default function Questionnaire(props) {
-// create a variable for each love type
-// display & iterate through each question
-// if the user selects option1, sum 1 to PT variable, etc.
-// At the end, the category with the most points will be chosen as the users love type
-// If there is a tie, use math.random
+export default function Questionnaire({ questions }) {
+    // create a variable for each love type
+    // display & iterate through each question
+    // if the user selects option1, sum 1 to PT variable, etc.
+    // At the end, the category with the most points will be chosen as the users love type
+    // If there is a tie, use math.random
 
     return (
         <>
-        {/* {props}.forEach(function displayQuestion() { */}
+            {/* {props}.forEach(function displayQuestion() { */}
             <div className="questionnaire">
                 <div className="title">
                     Discover Your Love Type
@@ -22,36 +22,45 @@ export default function Questionnaire(props) {
                     It means the MOST to me when my partner...
                     {/* Its most meaningful when my partner ... */}
                 </div>
-                <label className="option">
-                    <input type="radio" name="option" value="physicalTouchAnswer" />
-                    <span className="option1">
-                        {props.physicalTouchAnswer}
-                    </span>
-                </label>
-                <label className="option">
-                    <input type="radio" name="option" value="qualityTimeAnswer" />
-                    <span className="option2">
-                        {props.qualityTimeAnswer}
-                    </span>
-                </label>
-                <label className="option">
-                    <input type="radio" name="option" value="actsOfServiceAnswer" />
-                    <span className="option3">
-                        {props.actsOfServiceAnswer}
-                    </span>
-                </label>
-                <label className="option">
-                    <input type="radio" name="option" value="wordsOfAffirmationAnswer" />
-                    <span className="option4">
-                        {props.wordsOfAffirmationAnswer}
-                    </span>
-                </label>
-                <label className="option">
-                    <input type="radio" name="option" value="giftsAnswer" />
-                    <span className="option5">
-                        {props.giftsAnswer}
-                    </span>
-                </label>
+
+                {/* QUESTIONS START */}
+                {questions.map(question => {
+                    return (
+                        <>
+                            <label className="option">
+                                <input type="radio" name="option" value="physicalTouchAnswer" />
+                                <span className="option1">
+                                    {question.physicalTouchAnswer}
+                                </span>
+                            </label>
+                            <label className="option">
+                                <input type="radio" name="option" value="qualityTimeAnswer" />
+                                <span className="option2">
+                                    {question.qualityTimeAnswer}
+                                </span>
+                            </label>
+                            <label className="option">
+                                <input type="radio" name="option" value="actsOfServiceAnswer" />
+                                <span className="option3">
+                                    {question.actsOfServiceAnswer}
+                                </span>
+                            </label>
+                            <label className="option">
+                                <input type="radio" name="option" value="wordsOfAffirmationAnswer" />
+                                <span className="option4">
+                                    {question.wordsOfAffirmationAnswer}
+                                </span>
+                            </label>
+                            <label className="option">
+                                <input type="radio" name="option" value="giftsAnswer" />
+                                <span className="option5">
+                                    {question.giftsAnswer}
+                                </span>
+                            </label>
+                        </>
+                    )
+                })}
+
                 {/* Buttons */}
                 <div className="controls">
                     <button className="previous">Previous</button>
@@ -60,7 +69,7 @@ export default function Questionnaire(props) {
             </div>
 
             <div className="result"> </div>
-            
+
         </>
     )
     // )};
