e5ff322553009e92d5a27958a7387b144da5d260
Leslie Patino <lesliejpatino@gmail.com>
2022-03-11 15:54:17 -0800

    removed initial questions that were being used for testing

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -4,48 +4,6 @@ import './Question.css'
 import heart from '../../heart.png'
 import { FaCamera } from "react-icons/fa";
 import Questionnaire from '../Questions/Questionnaire.js'
-// import Modal from '../Modal/Modal.js'
-
-const questions = [
-  {
-    questionText: 'What is the capital of France?',
-    answerOptions: [
-      { answerText: 'New York', isCorrect: false },
-      { answerText: 'London', isCorrect: false },
-      { answerText: 'Paris', isCorrect: true },
-      { answerText: 'Dublin', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'Who is CEO of Tesla?',
-    answerOptions: [
-      { answerText: 'Jeff Bezos', isCorrect: false },
-      { answerText: 'Elon Musk', isCorrect: true },
-      { answerText: 'Bill Gates', isCorrect: false },
-      { answerText: 'Tony Stark', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'The iPhone was created by which company?',
-    answerOptions: [
-      { answerText: 'Apple', isCorrect: true },
-      { answerText: 'Intel', isCorrect: false },
-      { answerText: 'Amazon', isCorrect: false },
-      { answerText: 'Microsoft', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'How many Harry Potter books are there?',
-    answerOptions: [
-      { answerText: '1', isCorrect: false },
-      { answerText: '4', isCorrect: false },
-      { answerText: '6', isCorrect: false },
-      { answerText: '7', isCorrect: true },
-    ],
-  },
-];
-
-
 
 function Question() {
   const [name, setName] = useState('Mark');
