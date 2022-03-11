921dbc0693177503f213dc3e7aa0259ba20916fe
Leslie Patino <lesliejpatino@gmail.com>
2022-03-10 19:16:53 -0800

    changed name of keys within the loveTypeQuestions object

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -7,62 +7,63 @@
 // {loveTypeAnswers.actsOfService[i]}
 // {loveTypeAnswers.wordsOfAffirmation[i]}
 // {loveTypeAnswers.gifts[i]}
+
 const loveTypeQuestions = [
     {
-        'physical touch': 'pulls me close and cuddles me while we watch a movie',
-        'quality time': 'and I spend a weekend alone in a cabin',
-        'acts of service': 'completes a task for me that I have been dreading for weeks',
-        'words of affirmation': 'leaves me a thoughtful note in my bag',
-        'gifts': 'surprises me with an item I had mentioned I wanted weeks ago'
+        'physicalTouchAnswer': 'pulls me close and cuddles me while we watch a movie',
+        'qualityTimeAnswer': 'and I spend a weekend alone in a cabin',
+        'actsOfServiceAnswer': 'completes a task for me that I have been dreading for weeks',
+        'wordsOfAffirmationAnswer': 'leaves me a thoughtful note in my bag',
+        'giftsAnswer': 'surprises me with an item I had mentioned I wanted weeks ago'
     },
     {
-        'physical touch': 'greets me with a long hug',
-        'quality time': 'sits near me while I finish up work',
-        'acts of service': 'cooks me dinner after a busy day',
-        'words of affirmation': 'reminds me they are thinking of me in the middle of the day',
-        'gifts': 'buys concert tickets to go see my favorite artist'
+        'physicalTouchAnswer': 'greets me with a long hug',
+        'qualityTimeAnswer': 'sits near me while I finish up work',
+        'actsOfServiceAnswer': 'cooks me dinner after a busy day',
+        'wordsOfAffirmationAnswer': 'reminds me they are thinking of me in the middle of the day',
+        'giftsAnswer': 'buys concert tickets to go see my favorite artist'
     },
     {
-        'physical touch': 'grabs my hand during a walk',
-        'quality time': 'plans a device free evening for us',
-        'acts of service': 'wakes me up with breakfast in bed',
-        'words of affirmation': 'leaves a loving sticky note on my bathroom mirror',
-        'gifts': 'gets me a unique drawing of my favorite city'
+        'physicalTouchAnswer': 'grabs my hand during a walk',
+        'qualityTimeAnswer': 'plans a device free evening for us',
+        'actsOfServiceAnswer': 'wakes me up with breakfast in bed',
+        'wordsOfAffirmationAnswer': 'leaves a loving sticky note on my bathroom mirror',
+        'giftsAnswer': 'gets me a unique drawing of my favorite city'
     },
     {
-        'physical touch': 'gives me a massage as I get ready for bed',
-        'quality time': 'and I start a new tv series together',
-        'acts of service': 'shows up to my job with a cup of my favorite coffee',
-        'words of affirmation': 'speaks highly of me when introducing me to new friends',
-        'gifts': 'brings my cat/dog gourmet treats and toys'
+        'physicalTouchAnswer': 'gives me a massage as I get ready for bed',
+        'qualityTimeAnswer': 'and I start a new tv series together',
+        'actsOfServiceAnswer': 'shows up to my job with a cup of my favorite coffee',
+        'wordsOfAffirmationAnswer': 'speaks highly of me when introducing me to new friends',
+        'giftsAnswer': 'brings my cat/dog gourmet treats and toys'
     },
     {
-        'physical touch': 'plays with my hair',
-        'quality time': 'buys ingredients so we can cook a meal together',
-        'acts of service': 'plans a date night for us',
-        'words of affirmation': 'told me how good looking I was on an average day',
-        'gifts': 'creates a playlist of songs that I might love'
+        'physicalTouchAnswer': 'plays with my hair',
+        'qualityTimeAnswer': 'buys ingredients so we can cook a meal together',
+        'actsOfServiceAnswer': 'plans a date night for us',
+        'wordsOfAffirmationAnswer': 'told me how good looking I was on an average day',
+        'giftsAnswer': 'creates a playlist of songs that I might love'
     },
     {
-        'physical touch': 'rested their head on my lap',
-        'quality time': 'goes volunteering with me',
-        'acts of service': 'takes care of my every need while I lay in bed sick',
-        'words of affirmation': 'asks me to help with mundane tasks because they feel everything is more fun when they are with me',
-        'gifts': 'makes me a calendar full of pictures of our favorite memories together'
+        'physicalTouchAnswer': 'rested their head on my lap',
+        'qualityTimeAnswer': 'goes volunteering with me',
+        'actsOfServiceAnswer': 'takes care of my every need while I lay in bed sick',
+        'wordsOfAffirmationAnswer': 'asks me to help with mundane tasks because they feel everything is more fun when they are with me',
+        'giftsAnswer': 'makes me a calendar full of pictures of our favorite memories together'
     },
     {
-        'physical touch': 'holds on to me while we walk together',
-        'quality time': 'tried out a hobby that I love',
-        'acts of service': 'tossed my towel in the dryer while I showered so I have a warm towel to use when I exit',
-        'words of affirmation': 'tells me the things they admire about me',
-        'gifts': 'gave me an inspirational poster to hang in my new office'
+        'physicalTouchAnswer': 'holds on to me while we walk together',
+        'qualityTimeAnswer': 'tried out a hobby that I love',
+        'actsOfServiceAnswer': 'tossed my towel in the dryer while I showered so I have a warm towel to use when I exit',
+        'wordsOfAffirmationAnswer': 'tells me the things they admire about me',
+        'giftsAnswer': 'gave me an inspirational poster to hang in my new office'
     },
     {
-        'physical touch': 'rests their hand on mine while we drive',
-        'quality time': 'exercised with me',
-        'acts of service': 'meal prepped all of our meals for the week',
-        'words of affirmation': 'tells me why they appreciate me',
-        'gifts': 'bought me new shoes after I shared that I wanted to start running'
+        'physicalTouchAnswer': 'rests their hand on mine while we drive',
+        'qualityTimeAnswer': 'exercised with me',
+        'actsOfServiceAnswer': 'meal prepped all of our meals for the week',
+        'wordsOfAffirmationAnswer': 'tells me why they appreciate me',
+        'giftsAnswer': 'bought me new shoes after I shared that I wanted to start running'
     }
 ]
 
