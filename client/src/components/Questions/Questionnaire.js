e2f2f6d0542ea28126fca34cdf6391da8b1d30f6
Leslie Patino <lesliejpatino@gmail.com>
2022-03-10 22:18:18 -0800

    updated questionnaire file, added pseudocode that describes next steps and created a css file to style the questionnaire

--- client/src/components/Questions/Questionnaire.js
+++ client/src/components/Questions/Questionnaire.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import $ from 'jquery'
+import './Questionnaire.css'
 
-export default function Questionnaire() {
+
+export default function Questionnaire(props) {
+// create a variable for each love type
+// display & iterate through each question
+// if the user selects option1, sum 1 to PT variable, etc.
+// At the end, the category with the most points will be chosen as the users love type
+// If there is a tie, use math.random
+
     return (
         <>
+        {/* {props}.forEach(function displayQuestion() { */}
             <div className="questionnaire">
                 <div className="title">
                     Discover Your Love Type
@@ -10,30 +20,36 @@ export default function Questionnaire() {
 
                 <div id="question" className="question">
                     It means the MOST to me when my partner...
+                    {/* Its most meaningful when my partner ... */}
                 </div>
                 <label className="option">
                     <input type="radio" name="option" value="physicalTouchAnswer" />
                     <span className="option1">
+                        {props.physicalTouchAnswer}
                     </span>
                 </label>
                 <label className="option">
                     <input type="radio" name="option" value="qualityTimeAnswer" />
                     <span className="option2">
+                        {props.qualityTimeAnswer}
                     </span>
                 </label>
                 <label className="option">
                     <input type="radio" name="option" value="actsOfServiceAnswer" />
                     <span className="option3">
+                        {props.actsOfServiceAnswer}
                     </span>
                 </label>
                 <label className="option">
                     <input type="radio" name="option" value="wordsOfAffirmationAnswer" />
                     <span className="option4">
+                        {props.wordsOfAffirmationAnswer}
                     </span>
                 </label>
                 <label className="option">
                     <input type="radio" name="option" value="giftsAnswer" />
                     <span className="option5">
+                        {props.giftsAnswer}
                     </span>
                 </label>
                 {/* Buttons */}
@@ -44,6 +60,8 @@ export default function Questionnaire() {
             </div>
 
             <div className="result"> </div>
+            
         </>
     )
+    // )};
 }
