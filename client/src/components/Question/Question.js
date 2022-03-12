2cd69d121e4f24890fef414d13ad744669216d86
Leslie Patino <lesliejpatino@gmail.com>
2022-03-11 16:10:22 -0800

    commented out the code that is not currently being used and edited the contents of the questionnaire modal

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -5,34 +5,38 @@ import heart from '../../heart.png'
 import { FaCamera } from "react-icons/fa";
 import Questionnaire from '../Questions/Questionnaire.js'
 
-function Question() {
+export default function Question() {
   const [name, setName] = useState('Mark');
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-  const [start, setStart] = useState(false);
   const [showContent, setShowContent] = useState(false)
 
-  const handleClickStart = () => {
-    setShowContent( true )
+  // this will open the modal upon click
+  const openModal = () => {
+    setShowContent(true)
   }
 
-  const handleClose = () => {
-    setShowContent( false )
+  // this will close the modal upon click
+  const closeModal = () => {
+    setShowContent(false)
   }
 
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
+  // const [currentQuestion, setCurrentQuestion] = useState(0);
+  // const [showScore, setShowScore] = useState(false);
+  // const [score, setScore] = useState(0);
+  // const [start, setStart] = useState(false);
 
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
-    }
-  };
+
+  // const handleAnswerOptionClick = (isCorrect) => {
+  //   if (isCorrect) {
+  //     setScore(score + 1);
+  //   }
+
+  // const nextQuestion = currentQuestion + 1;
+  // if (nextQuestion < questions.length) {
+  //   setCurrentQuestion(nextQuestion);
+  // } else {
+  //   setShowScore(true);
+  // }
+
 
   return (
     <div className='question'>
@@ -51,34 +55,22 @@ function Question() {
             <div className='lower-container'>
               <div>
                 <h3>{name}</h3>
-                <button className='button'>{name} 's Love Language: {score}<br /> {questions[0].questionText}</button>
-                {start ? (
-                  <div>
-                    <button className='button'>{score} 's Love Language:<br /> {questions.length}</button>
-                  </div>
-                ) : (
-                  <>
-                    {/* <button className='button' onClick={handleClickStart}>Please answer some love language questions</button> */}
-                    <button className='button' onClick={handleClickStart}>Please answer some love language questions</button>
-                    <Modal show={showContent} onHide={handleClose}>
-                      <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body> <Questionnaire /></Modal.Body>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                          Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                          Save Changes
-                        </Button>
-                      </Modal.Footer>
-                    </Modal>
+                <br />
+                <button className='button'>{name} 's Love Language: <br /> </button>
 
-                  </>
-                )}
+                {/* Questionnaire Modal */}
+                <button className='button' onClick={openModal}>Discover Your Love Language!</button>
+                <Modal show={showContent} onHide={closeModal}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Discover Your Love Language</Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <Questionnaire />
+                  </Modal.Body>
+                </Modal>
+                
               </div>
-
             </div>
 
           </div>
@@ -86,45 +78,38 @@ function Question() {
             <div className='px-5' >
               <img src={heart} alt='heart' />
             </div>
-            <div className='context'>
-              {showContent && <div className='app'>
-                {showScore ? (
-                  <div className='score-section vstack'>
-                    <div>
-                      How to make {name} Feel More Loved
-                    </div>
-                    <div className='answer-section'>
-                      {questions[2].answerOptions.map((answerOption) => (
-                        <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-                      ))}
-                    </div>
-                  </div>
-                ) : (
-                  <>
-                    <div className='question-section'>
-                      <div className='question-count'>
-                        <span>Question {currentQuestion + 1}</span>/{questions.length}
+
+            {/* <div className='context'>
+                {showContent && <div className='app'>
+                  {showScore ? (
+                    <div className='score-section vstack'>
+                      <div>
+                        How to make {name} Feel More Loved
                       </div>
-                      <div className='question-text'>{questions[currentQuestion].questionText}</div>
-                    </div>
-                    <div className='answer-section'>
-                      {questions[currentQuestion].answerOptions.map((answerOption) => (
-                        <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                      <div className='answer-section'>
+                        {questions[2].answerOptions.map((answerOption) => (
+                          <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                        ))}
+                      </div>
+                    </div> */}
 
-                      ))}
-                    </div>
-                  </>
-                )}
-              </div>
-              }
-            </div>
+            {/* <div className='question-section'>
+                        <div className='question-count'>
+                          <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                      </div>
+                      <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOption) => (
+                          <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+
+                        ))}
+                      </div> */}
           </div>
+
         </div>
-
-
       </div>
     </div>
   )
-}
+};
 
-export default Question
