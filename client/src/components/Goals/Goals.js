bd5ed739e40be0f654d938d9a2706fed3410cb07
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-12 14:46:59 -0800

    created title and scroll overflow for goals list

--- client/src/components/Goals/Goals.js
+++ client/src/components/Goals/Goals.js
@@ -1,14 +1,32 @@
 import React from "react";
 
+
 const Goals = () => {
   return (
   <div>
-    
+    <h1 style={title}> Goals list </h1>
+   <div style={style}>
+     <p> test </p>
+     <p> test 1 </p>
+     <p> test  2</p>
+     <p> test 3  </p>
+     <p> test  4 </p>
+     <p> test  5 </p>
+  </div>
   </div>
   );
 };
 
 const style = {
-  position: "relative",
+  "backgroundColor": "lightblue",
+  "width": "auto",
+  "height": "280px",
+  "overflow": "scroll"
 };
+
+const title = {
+  "color": "black"
+}
+
+
 export default Goals;
