c1780659df311c1a79aa0a5e6c26e1d01766163e
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-12 20:07:50 -0800

    added use state count in counter.js

--- client/src/components/Goals/Counter.js
+++ client/src/components/Goals/Counter.js
@@ -1,6 +1,17 @@
-import React from "react";
+import {useState} from "react";
+import Button from "./Button";
+
 
 const Counter=()=>{
+    const [count, setCount] = useState(0);
+
+    let incrementCount = () => {
+        setCount(count + 1);
+      };
+    
+      let decrementCount = () => {
+        setCount(count - 1);
+      };
     return(
         <>
         </>
